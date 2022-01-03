3547ca787745f93338ce80544421eb781eb4cdc6
kyoun9min <rtong77@naver.com>
2022-01-03 21:06:07 +0900

    SLUB-3 Team Repository 작성

--- spammayo-common/src/main/java/io/samkime/spammayo/repository/TeamRepository.java
+++ spammayo-common/src/main/java/io/samkime/spammayo/repository/TeamRepository.java
@@ -1,4 +1,13 @@
 package io.samkime.spammayo.repository;
 
-public interface TeamRepository {
+import io.samkime.spammayo.entity.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Optional<Team> findByName(String name);
 }
