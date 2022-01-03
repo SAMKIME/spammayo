b7e4ba9a8bbe261ee4aac93945f1bba4d32cbc66
kyoun9min <rtong77@naver.com>
2022-01-03 20:13:47 +0900

    SLUB-3 Team Entity 작성

--- spammayo-common/src/main/java/io/samkime/spammayo/entity/Team.java
+++ spammayo-common/src/main/java/io/samkime/spammayo/entity/Team.java
@@ -1,4 +1,33 @@
 package io.samkime.spammayo.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Accessors(chain = true)
+@Entity
 public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String member;
+
+    private String area;
+
+    private String sportsType;
 }
