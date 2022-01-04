574c9b6d10a2e011239489d413e93d98f5683621
kyoun9min <rtong77@naver.com>
2022-01-04 23:48:02 +0900

    SLUB-3 Entity 수정

--- spammayo-common/src/main/java/io/samkime/spammayo/entity/Team.java
+++ spammayo-common/src/main/java/io/samkime/spammayo/entity/Team.java
@@ -1,22 +1,17 @@
 package io.samkime.spammayo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Accessors(chain = true)
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+@Getter
+@EqualsAndHashCode(of = {"id"})
 public class Team {
 
     @Id
@@ -30,4 +25,12 @@ public class Team {
     private String area;
 
     private String sportsType;
+
+    @Builder
+    public Team(String name, String member, String area, String sportsType) {
+        this.name = name;
+        this.member = member;
+        this.area = area;
+        this.sportsType = sportsType;
+    }
 }
